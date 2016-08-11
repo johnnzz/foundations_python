d8b63dd1eaf72b634bdbae3aa6858dbfd59204eb
John Navitsky <johnn@cray.com>
2016-08-10 23:04:52 -0700

    added constructor defaults

--- module8/lab8-2.py
+++ module8/lab8-2.py
@@ -6,7 +6,7 @@ class person():
     def to_string(self):
         return self.firstname + " " + self.lastname
 
-    def __init__(self, firstname, lastname):
+    def __init__(self, firstname = "", lastname = ""):
         self.firstname = firstname
         self.lastname = lastname
 
@@ -14,3 +14,6 @@ myguy = person("fred", "jones")
 
 print(myguy.to_string())
 
+otherguy = person(lastname = "smith", firstname = "sue")
+
+print(otherguy.to_string())
