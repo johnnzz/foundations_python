997dd644ea4726f17c5765e3aa522a0c5433fa52
John Navitsky <johnn@cray.com>
2016-08-10 22:55:36 -0700

    lab 8-2, constructor example

---
+++ module8/lab8-2.py
@@ -0,0 +1,16 @@
+class person():
+
+    firstname = None
+    lastname = None
+
+    def to_string(self):
+        return self.firstname + " " + self.lastname
+
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+myguy = person("fred", "jones")
+
+print(myguy.to_string())
+
