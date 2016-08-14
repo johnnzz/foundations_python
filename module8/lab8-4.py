91ff9d1c75b03d7d0be7bf51b3daccc5294860f4
John Navitsky <john@navitsky.org>
2016-08-13 21:34:10 -0700

    add __str__ method override

---
+++ module8/lab8-4.py
@@ -0,0 +1,55 @@
+class person():
+
+    # fields
+
+    #firstname = None
+    #lastname = None
+
+    # constructor
+
+    def __init__(self, firstname = "", lastname = ""):
+        # use properties
+        self.firstname = firstname
+        self.lastname = lastname
+
+    # properties
+
+    # getter or accessor
+    @property
+    def firstname(self):
+        return self.__firstname
+
+    # settor or mutator
+    @firstname.setter
+    def firstname(self, value):
+        self.__firstname = value.strip().capitalize()
+
+    # getter or accessor
+    @property
+    def lastname(self):
+        return self.__lastname
+
+    # settor or mutator
+    @lastname.setter
+    def lastname(self, value):
+        self.__lastname = value.strip().capitalize()
+
+    # methods
+
+    def to_string(self):
+        return self.firstname + " " + self.lastname
+
+    # override __str__ to call to_string method
+    def __str__(self):
+        return self.to_string()
+
+
+myguy = person("fred  ", "  jones")
+
+print(myguy.to_string())
+
+otherguy = person(lastname = "smIth", firstname = "sue")
+
+print(otherguy.to_string())
+
+print("__str__ prints:",myguy)
