ab6d0dba472ef3d29e55fb2e61cd8a11075b5a4d
John Navitsky <john@navitsky.org>
2016-08-13 19:00:10 -0700

    with accessors and mutators

--- module8/lab8-3.py
+++ module8/lab8-3.py
@@ -2,16 +2,36 @@ class person():
 
     # fields
 
-    firstname = None
-    lastname = None
+    #firstname = None
+    #lastname = None
 
     # constructor
 
     def __init__(self, firstname = "", lastname = ""):
-        self.firstname = firstname
-        self.lastname = lastname
+        self.__firstname = firstname
+        self.__lastname = lastname
 
     # properties
+
+    # getter or accessor
+    @property
+    def firstname(self):
+        return self.__firstname.strip().capitalize()
+
+    # settor or mutator
+    @firstname.setter
+    def firstname(self, value):
+        self.__firstname = value
+
+    # getter or accessor
+    @property
+    def lastname(self):
+        return self.__lastname.strip().capitalize()
+
+    # settor or mutator
+    @lastname.setter
+    def lastname(self, value):
+        self.__lastname = value
 
     # methods
 
